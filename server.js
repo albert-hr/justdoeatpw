8ae2ba776da5da999a1c5142dd1bae2182d31046
albert-hr <alberthrdev@gmail.com>
2026-06-09 08:52:32 -0300

    Reorganizando o server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const pool = require('./utils/db');
 const PgSession = require('connect-pg-simple')(session);
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,41 +38,5 @@ app.get('/api/me', (req, res) => {
   res.json({ user: req.session.usuario });
 });
 
-// Rota protegida do admin
-app.get('/admin.html', (req, res) => {
-  if (!req.session.usuario || req.session.usuario.perfil !== 'admin') {
-    return res.redirect('/login.html?erro=acesso-negado');
-  }
-  res.sendFile(path.join(__dirname, 'views', 'admin.html'));
-});
-
-// Rotas protegidas de restaurante
-app.get('/dashboardv2.html', (req, res) => {
-  if (!req.session.usuario || req.session.usuario.perfil !== 'Restaurante') {
-    return res.redirect('/login.html?erro=acesso-negado');
-  }
-  res.sendFile(path.join(__dirname, 'views', 'dashboardv2.html'));
-});
-
-// Rota protegida de cliente
-app.get('/meu-perfil.html', (req, res) => {
-  if (!req.session.usuario) {
-    return res.redirect('/login.html?erro=login-obrigatorio');
-  }
-  res.sendFile(path.join(__dirname, 'views', 'meu-perfil.html'));
-});
-
-// Servir demais HTMLs
-app.get('/:page.html', (req, res) => {
-  const file = path.join(__dirname, 'views', req.params.page + '.html');
-  res.sendFile(file, (err) => {
-    if (err) res.status(404).send('Página não encontrada');
-  });
-});
-
-// Página inicial
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-module.exports = (req, res) => app(req, res);
+// DEBUG TEMPORÁRIO - remover após usar
+app.get('/api/debug-session', (req, res) => {
